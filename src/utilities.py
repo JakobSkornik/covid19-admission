80f4f7910e315fcf26375555ea454f3445d7f03c
skornikjakob <skornikjakobs@gmail.com>
2021-12-27 20:01:59 +0100

    Temp

--- src/utilities.py
+++ src/utilities.py
@@ -1,8 +1,8 @@
 import json
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn import metrics
 
 def load_xlsx(path: str) -> pd.DataFrame:
     """
